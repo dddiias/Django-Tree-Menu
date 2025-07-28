c763f9161f6144a81dd44fe3f7aab7a7fb2c8dc0
dddiias <115692320+dddiias@users.noreply.github.com>
2025-07-29 02:18:49 +0500

    Add /products/ route and demo view/template (#2)
    * feat(menu): compute safe href in tag; stop calling {% url %} in template
    
    * fix(pages): add /products/ route and demo view/template

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import HomeView, AboutView, ContactsView, ProductView
+from pages.views import HomeView, AboutView, ContactsView, ProductView, ProductsListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView.as_view(), name="home"),
     path("about/", AboutView.as_view(), name="about"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("products/", ProductsListView.as_view(), name="products"),
     path("product/<slug:slug>/", ProductView.as_view(), name="product"),
 ]
